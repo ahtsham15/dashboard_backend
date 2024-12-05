3e08634007357b03303fb61c5d2c69abdaa49591
Ahtsham <ahtshamsandhu05@gmail.com>
2024-12-05 19:19:09 +0500

    myfinance: create signup api

--- myfinance/views.py
+++ myfinance/views.py
@@ -12,6 +12,19 @@ class UserList(APIView):
             "status": "success",
             "data": serializer.data
         })
+    def post(self, request):
+        data = request.data
+        serializer = UserSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({
+                "status": False,
+                "message": serializer.errors
+            })
+        serializer.save()
+        return Response({
+            "status": True,
+            "message": "User created successfully"
+        })
     
 
 class LoginView(APIView):
